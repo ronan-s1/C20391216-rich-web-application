3b00bcf350607894becee4896d9c1492daffd4ee
ronan-s1 <singpurwala13@gmail.com>
2023-10-11 02:21:45 +0100

    add q2 + rename script to scripts

---
+++ lab2/problem-2/scripts.js
@@ -0,0 +1,37 @@
+// List all of the post titles having more than six words
+async function listPostTitlesWithMoreThanSixWords() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+
+    const titlesWithMoreThanSixWords = posts
+        .filter(post => post.title.split(" ").length > 6)
+        .map(post => post.title);
+
+    const titlesList = document.getElementById("titlesList");
+    titlesWithMoreThanSixWords.forEach(title => {
+        const li = document.createElement("li");
+        li.textContent = title;
+        titlesList.appendChild(li);
+    });
+}
+
+listPostTitlesWithMoreThanSixWords();
+
+// Show a word frequency map for all of the body contents of the posts
+async function showWordFrequencyMap() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+
+    const bodyText = posts.map(post => post.body);
+    const words = bodyText.join(" ").split(" ")
+
+    const wordFrequencyMap = words.reduce((frequencyMap, word) => {
+        frequencyMap[word] = (frequencyMap[word] || 0) + 1;
+        return frequencyMap;
+    }, {});
+
+    const wordFrequencyResult = document.getElementById("wordFrequencyResult");
+    wordFrequencyResult.textContent = JSON.stringify(wordFrequencyMap, null, 2);
+}
+
+showWordFrequencyMap();
